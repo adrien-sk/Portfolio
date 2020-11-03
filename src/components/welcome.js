34f06cbb5dccf5cc6275202bb36917360debcfb0
Adrien <skrzypczak.adrien@gmail.com>
2020-11-04 00:21:34 +0100

    Fix a Word

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -11,7 +11,7 @@ const Welcome = () =>{
 					<h3>a Full-Stack Software Engineer</h3>
 					<p>I have 7 years of experience in different languages and platforms, but a longer affinity for Computer Science.</p>
 					<p>I'm working a lot with C# .Net for Web Apps, but also in Unity to work on Video Games on my free time.</p>
-					<p>Recently focusing on one of my first language : Javascript and it's friends (NodeJS, React, Redux, Next.JS, Express, ...)</p>
+					<p>Recently focusing on one of my first language : Javascript and its friends (NodeJS, React, Redux, Next.JS, Express, ...)</p>
 					<p>I'm also having the chance to pursue a Master Degree and expand on my Computer Science knowledges.</p>
 				</div>
 				<img src={profileImage} alt="Profile"/>
