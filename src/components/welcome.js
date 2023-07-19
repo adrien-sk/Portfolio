6ad0774dd81bbe8a29e558a1d197c0c519663a99
Adrien S <skrzypczak.adrien@gmail.com>
2023-07-19 15:39:04 +0200

    Update welcome.js

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -10,11 +10,13 @@ const Welcome = () =>{
 				<div class="profile-description">
 					<h3>Hello, I'm Adrien,</h3>
 					<h3>a Full-Stack Software Engineer</h3>
-					<p>I have ~<strong>7 years of experience</strong> in different languages and platforms, but a longer affinity for <strong>Computer Science</strong>.</p>
+					<p>I have ~<strong>10 years of experience</strong> in different languages and platforms, but a longer affinity for <strong>Computer Science</strong>.</p>
 					<br />
-					<p>I'm working a lot with <strong>C# .Net</strong> for <strong>Web Apps</strong>, but also in Unity to work on Video Games on my free time.</p>
-					<p>Recently focusing on one of my first language : <strong>Javascript</strong> and its friends (<strong>NodeJS</strong>, <strong>React</strong>, <strong>Redux</strong>, <strong>Next.JS</strong>, <strong>Express</strong>, ...)</p>
+					<p>I'm working with multiples Front-end technologies like <strong>HTML</strong>, <strong>CSS</strong>, <strong>Typescript</strong> and its latest friends (<strong>NodeJS</strong>, <strong>Angular</strong>, <strong>React</strong>, <strong>Next.JS</strong>, <strong>Express</strong>, ...)</p>
 					<br />
+					<p>Also proficient with <strong>C#</strong>, working with <strong>.Net Core</strong>, <strong>SQL Server</strong> and <strong>Oracle</strong>. </p>
+					<br />
+					<p>Currently digging into <strong>Go</strong> Language, I've also worked with <strong>PHP</strong>, <strong>Python</strong>, <strong>Java</strong>, ...</p>
 					<p>Very <strong>Curious</strong> and <strong>Interested</strong> in new technologies, I love <strong>Learning</strong>, <strong>Discovering</strong>, and <strong>Building things</strong>.</p>
 					<br />
 					<p>I focus on developing <strong>Quality</strong> and <strong>Functional</strong> products.</p>
